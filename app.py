bddfbc04043551a56cb3f78b0dd27aa37459182b
nnd901 <nhutminhd@gmail.com>
2024-12-28 18:01:02 -0600

    updated CORS for testing

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, request, jsonify
 from translate_test import translate_prompt, send_prompt
 from dotenv import load_dotenv
 import os
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 load_dotenv()
 port=os.getenv("PORT")
 app=Flask(__name__)
 # CORS(app, origins="https://translate-frontend-032f.onrender.com/", methods=["GET", "POST"])
-CORS(app)
+# CORS(app)
+
 @app.route('/')
 
 def index():
     return render_template('index.html')
 
 @app.route('/message', methods=['POST'])
+@cross_origin(origin='https://translate-frontend-032f.onrender.com')
 def translate():
     data = request.get_json()
     message = data.get('message', '')
